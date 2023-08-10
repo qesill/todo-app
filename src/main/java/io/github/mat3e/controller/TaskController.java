5690263897b4e7eae812ab8b5234ce1281ce0377
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-08-10 15:17:29 +0200

    ? > List<Task

--- src/main/java/io/github/mat3e/controller/TaskController.java
+++ src/main/java/io/github/mat3e/controller/TaskController.java
@@ -1,11 +1,15 @@
 package io.github.mat3e.controller;
 
+import io.github.mat3e.model.Task;
 import io.github.mat3e.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RepositoryRestController
 class TaskController {
@@ -16,10 +20,16 @@ class TaskController {
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
-    @GetMapping("/tasks")
-    ResponseEntity<?> readAllTasks() {
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the task!");
         return ResponseEntity.ok(repository.findAll());
+    }
+    @GetMapping("/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
 
