44015fa946cd1c5cd6efb4c9de35355e6298aeb0
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-08-10 14:43:47 +0200

    GetMapping insted of RequestMapping

--- src/main/java/io/github/mat3e/controller/TaskController.java
+++ src/main/java/io/github/mat3e/controller/TaskController.java
@@ -5,8 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
 class TaskController {
@@ -17,7 +16,7 @@ class TaskController {
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @GetMapping("/tasks")
     ResponseEntity<?> readAllTasks() {
         logger.warn("Exposing all the task!");
         return ResponseEntity.ok(repository.findAll());
